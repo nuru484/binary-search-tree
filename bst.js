7b34f3f2a4b84a15928601d77b4b16fb42afad8f
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.comm>
2024-02-04 11:52:16 -0800

    Added Tree class and a buildTree method

--- bst.js
+++ bst.js
@@ -1,7 +1,24 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(this.removeDuplicates(this.sortArray(array)));
+  }
+
+  buildTree(array) {
+    if (array.length === 0) {
+      return null;
     }
+    const middleIndex = Math.floor(array.length / 2);
+    const root = new Node(array[middleIndex]);
+    root.left = this.buildTree(array.slice(0, middleIndex));
+    root.right = this.buildTree(array.slice(middleIndex + 1));
+    return root;
+  }
 }
